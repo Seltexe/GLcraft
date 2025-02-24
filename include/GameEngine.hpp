dfcb65ed4fe9ac160103c69607074f9036bf3e8d
Adrien F <powa34480@gmail.com>
2025-02-24 11:09:00 +0100

    up

--- include/GameEngine.hpp
+++ include/GameEngine.hpp
@@ -1,5 +1,14 @@
 #pragma once
 #include "RenderingEngine.hpp"
+#include "Shader.hpp"
+#include "fileManager.hpp"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#ifndef M_PI_2
+#define M_PI_2 1.57079632679489661923
+#endif
 
 
 
@@ -23,8 +32,14 @@ namespace Ge
 
 	private:
 
+		Shader simpleShader;
 		bool m_initialized = false;
 		RenderingEngine* mp_rendering_engine;
+
+		unsigned int vao, vbo, ebo;
+		glm::mat4 model;
+		glm::mat4 view;
+		glm::mat4 projection;
 	};
 
 
--- include/pch.h
+++ include/pch.h
@@ -5,5 +5,7 @@
 #include <GLFW/glfw3.h>
 #include "imgui.h"
 #include <stdexcept>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #endif // !PCH_H
